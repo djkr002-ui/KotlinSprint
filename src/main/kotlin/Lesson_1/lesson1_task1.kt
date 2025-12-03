d1f07d7f549601a3dda1cb9b28b4591658c65682
Mari <djkr002@mail.ru>
2025-12-03 17:17:13 +0500

    KS-1-1-variable names

---
+++ src/main/kotlin/Lesson_1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package org.example.Lesson_1
+
+fun main() {
+    val numberOfOrders = 75
+    val thankYouMessage = "Thank you for your purchase in our online store"
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+}
