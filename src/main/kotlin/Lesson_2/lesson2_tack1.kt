c90ee66f6a38622f51ce053e7cf371199b0d0d9c
Mari <djkr002@mail.ru>
2025-12-04 18:25:43 +0500

    KS-2-1-changed the name and added format

--- src/main/kotlin/Lesson_2/lesson2_tack1.kt
+++ src/main/kotlin/Lesson_2/lesson2_tack1.kt
@@ -1,14 +1,14 @@
 package org.example.Lesson_2
 
 fun main() {
-    val student = 4
+    val studentsCount = 4
     val studentAssessment1 = 3
     val studentAssessment2 = 4
     val studentAssessment3 = 3
     val studentAssessment4 = 5
 
     val sumOfRatings = studentAssessment1 + studentAssessment2 + studentAssessment3 + studentAssessment4
-    val averageScore = sumOfRatings / student.toDouble()
+    val averageScore = sumOfRatings / studentsCount.toDouble()
 
-    println(averageScore)
+    println(String.format("%02d", averageScore))
 }
