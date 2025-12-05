ca5425d0bd6bd66825dc94f4272a897dc49a8dd0
Mari <djkr002@mail.ru>
2025-12-05 17:00:45 +0500

    KS-3-2-output and overwriting of variables

---
+++ src/main/kotlin/Lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_3
+
+fun main() {
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $patronymic, $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $patronymic, $age")
+}
