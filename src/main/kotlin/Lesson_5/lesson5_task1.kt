a953ca4d68025a499404fb4314da91b1d899db7a
Mari <djkr002@mail.ru>
2025-12-07 20:35:00 +0500

    KS-5-1-variable names

--- src/main/kotlin/Lesson_5/lesson5_task1.kt
+++ src/main/kotlin/Lesson_5/lesson5_task1.kt
@@ -1,15 +1,15 @@
 package org.example.Lesson_5
 
 fun main() {
-    val a = (1..9).random()
-    val b = (1..9).random()
+    val number1 = (1..9).random()
+    val number2 = (1..9).random()
 
     println("Решите пример, для входа в систему:")
-    println("Сколько будет $a+$b?")
+    println("Сколько будет $number1+$number2?")
 
     val userResponse = readln().toInt()
 
-    if (userResponse == a + b) {
+    if (userResponse == number1 + number2) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
