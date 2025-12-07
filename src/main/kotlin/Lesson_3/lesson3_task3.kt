c8487d185e1c6a22b15fa74891f72319eb521ec1
Mari <djkr002@mail.ru>
2025-12-07 20:43:20 +0500

    KS-3-3-table output

--- src/main/kotlin/Lesson_3/lesson3_task3.kt
+++ src/main/kotlin/Lesson_3/lesson3_task3.kt
@@ -3,16 +3,5 @@ package org.example.Lesson_3
 fun main() {
     val number = 6
 
-    val table = """
-        $number * 1 = 6
-        $number * 2 = 12
-        $number * 3 = 18
-        $number * 4 = 24
-        $number * 5 = 30
-        $number * 6 = 36
-        $number * 7 = 42
-        $number * 8 = 48
-        $number * 9 = 54
-       """.trimIndent()
-    println(table)
+    println((1..9).joinToString("\n") { "$number x $it = ${number * it}" })
 }
